5be8f4cc4b399fd3b0640f6f9808207ab95f87ac
MathieuRalamboson <ralamboson@et.esiea.fr>
2019-03-26 20:10:26 +0100

    Initial commit

---
+++ app/src/main/java/com/MainActivity.java
@@ -0,0 +1,44 @@
+package com;
+
+import com.github.vincebrees.esiea32.R;
+import com.model.Pokemon;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private MainController controller;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.my_recycler_view);
+
+        controller = new MainController(this);
+        controller.onStart();
+
+        // use this setting to
+        // improve performance if you know that changes
+        // in content do not change the layout size
+        // of the RecyclerView
+    }
+
+    public void showList(List<Pokemon> input){
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        // define an adapter
+        mAdapter = new MyAdapter(input);
+        recyclerView.setAdapter(mAdapter);
+    }
+}
---
+++ app/src/main/java/com/MainController.java
@@ -0,0 +1,61 @@
+package com;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import com.model.Pokemon;
+import com.model.RestPokemonResponse;
+
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainController {
+
+    private MainActivity activity;
+
+    public MainController(MainActivity mainActivity) {
+        this.activity = mainActivity;
+    }
+
+    public void onStart(){
+
+        //Pour ceux qui veulent aller plus loin
+        //Singleton
+        //Pour ceux qui veulent aller encore plus loin
+        // Injection de dépendances
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://pokeapi.co/api/v2/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        RestPokemonApi restPokemonApi = retrofit.create(RestPokemonApi.class);
+
+        Call<RestPokemonResponse> call = restPokemonApi.getListPokemon();
+        call.enqueue(new Callback<RestPokemonResponse>() {
+            @Override
+            public void onResponse(Call<RestPokemonResponse> call,
+                    Response<RestPokemonResponse> response) {
+                RestPokemonResponse restPokemonResponse = response.body();
+                List<Pokemon> listPokemon = restPokemonResponse.getResults();
+                activity.showList(listPokemon);
+            }
+
+            @Override
+            public void onFailure(Call<RestPokemonResponse> call, Throwable t) {
+                Log.d("ERROR", "Api Error");
+            }
+        });
+    }
+}
