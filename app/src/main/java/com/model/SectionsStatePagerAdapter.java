e5b62d5981175dc9ead7eb25eef1b6460f6a8775
MathieuRalamboson <ralamboson@et.esiea.fr>
2019-03-27 14:37:39 +0100

    Second Activity Debut 1.1

---
+++ app/src/main/java/com/model/SectionsStatePagerAdapter.java
@@ -0,0 +1,34 @@
+package com.model;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionsStatePagerAdapter extends FragmentStatePagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mfragmentTitleList = new ArrayList<>();
+
+    public SectionsStatePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    private void addFragment(Fragment fragment,String title){
+        mFragmentList.add(fragment);
+        mfragmentTitleList.add(title);
+    }
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}
+
